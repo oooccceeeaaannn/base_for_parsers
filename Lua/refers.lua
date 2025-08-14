9bbe927f281423e8b917cad2ef9878a214eaa117
oooccceeeaaannn <ocean18071@qq.com>
2025-08-14 18:42:34 +0800

    refers now works

--- Lua/refers.lua
+++ Lua/refers.lua
@@ -1,22 +1,23 @@
 condlist['refers'] = function(params,checkedconds,checkedconds_,cdata)
 	for i, j in pairs(params) do
 		local passedthischeck = false
-		local _params = " " .. j
+		local _params = tostring(j)
+		local unitname
 		if cdata.unitid ~= 2 then
-			local unitname = mmf.newObject(cdata.unitid).strings[UNITNAME]
+			if cdata.unitid ~= 1 then
+				unitname = mmf.newObject(cdata.unitid).strings[UNITNAME]
+			else
+				unitname = "level"
+			end
 		else
-			local unitname = "empty"
+			unitname = "empty"
 		end
-
-		_params = string.sub(_params, 2)
         
-		if get_ref(unitname) == _params then
+		if (get_ref(unitname) == _params) and (is_str_special_prefixed(unitname)) then
             passedthischeck = true
         end
 
 		if hasfeature(unitname, "is", "word", cdata.unitid) and (unitname == _params) then
-			passedthischeck = true
-		elseif hasfeature(unitname, "is", "symbol", cdata.unitid) and (unitname == _params) then
 			passedthischeck = true
 		end
 		if not passedthischeck then return false, checkedconds end
